38086bd503eb59f3c168168520d15b68d1986976
FrederikSchow <Frederikmorthorst123@yahoo.com>
2026-02-17 10:45:29 +0100

    task 1

---
+++ task1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import re
+
+# Download nødvendige pakker fra NLTK
+nltk.download('punkt')
+nltk.download('stopwords')
+
